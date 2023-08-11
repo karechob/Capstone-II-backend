0ae446afd891ccd92f6ec5499bb74f5fd1b22505
yuzchen7 <yuzchen7@gmail.com>
2023-08-11 17:05:11 -0400

    init : add seesion tracker

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const cors = require("cors");
+const session = require("express-session");
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const store = new sequelizeStore({ db });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(
+  session({
+    secret: "capstoneii",
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 3 * 1000 * 60 * 60 },
+    httpOnly: true,
+  })
+);
 
 const setupRoutes = () => {
     app.use("/api", require("./api"));
@@ -22,6 +37,7 @@ const runServer = async (port) => {
 };
 
 const configureApp = async (port) => {
+    await store.sync();
     setupRoutes();
     return runServer(port);
 };
