564adfc3061cbed4588bf300a268d7e8b0699ec1
yuzchen7 <yuzchen7@gmail.com>
2023-08-11 17:35:30 -0400

    fix : add exception handle in commit request

--- api/github.js
+++ api/github.js
@@ -7,12 +7,17 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_ACCESS_TOKEN
 });
 
-router.get("/", async (req, res) => {
-  const result = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-    owner: 'yuzchen7',
-    repo: 'ttp_crud_backend',
-  })
-  res.send(result.data);
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+      owner: 'yuzchen7',
+      repo: 'ttp_crud_backend',
+    });
+    res.send(result.data);
+  } catch (err) {
+    res.status(500).json({message : 'get require commit failed'});
+    next(err);
+  }
 });
 
 module.exports = router;
