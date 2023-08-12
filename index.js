d54e99ccd884b98e0bb0a2d498ebf742208bf1b8
Kaifeng <1536771545kai@gmail.com>
2023-08-11 22:44:24 -0400

    modify index.js

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const express = require("express");
 const app = express();
-const db = require("./db");
 const cors = require("cors");
-const session = require("express-session");
-const sequelizeStore = require("connect-session-sequelize")(session.Store);
-
-const store = new sequelizeStore({ db });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-app.use(
-  session({
-    secret: "capstoneii",
-    store: store,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 3 * 1000 * 60 * 60 },
-    httpOnly: true,
-  })
-);
 
 const setupRoutes = () => {
     app.use("/api", require("./api"));
@@ -30,14 +14,12 @@ const setupRoutes = () => {
 };
 
 const runServer = async (port) => {
-    await db.sync({ force: true }); // test purpose only
     app.listen(port, () => {
         console.log(`server is running on port 8080`);
     });
 };
 
 const configureApp = async (port) => {
-    await store.sync();
     setupRoutes();
     return runServer(port);
 };
