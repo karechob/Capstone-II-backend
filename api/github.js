0dd79b1da4e00e94c29b85a3d8564089cacfb13d
eluzung <luzungelaine@gmail.com>
2023-08-22 07:22:51 -0400

    feat: Add rework router to connect api filtered data to the frontend
    fix: Modify rework filter function with more conditionals to get correct data

--- api/github.js
+++ api/github.js
@@ -624,8 +624,10 @@ async function reworkFilter(ownerOfRepo, repository, shaString) {
       sha: shaString,
     }
   );
+
   commitStats.data.files.forEach((fileData) => {
-    if (fileData.status === "modified") {
+    //Indicates that code in files were modified
+    if (fileData.status === "modified" && fileData.deletions !== 0) {
       const fileObject = {
         status: fileData.status,
         changes: fileData.changes,
@@ -637,13 +639,57 @@ async function reworkFilter(ownerOfRepo, repository, shaString) {
     }
   });
 
-  const dataObject = {
-    date: commitStats.data.commit.committer.date,
-    stats: commitStats.data.stats,
-    numFiles: commitStats.data.files.length,
-    files: files,
-  };
-  return dataObject;
+  if (files.length > 0 && commitStats.data.stats.deletions !== 0) {
+    const dataObject = {
+      date: commitStats.data.commit.committer.date.split("T")[0],
+      files: files,
+    };
+
+    return dataObject;
+  }
 }
+
+router.post("/rework", async (req, res, next) => {
+  const { owner, repo } = req.body;
+  try {
+    const filteredDataArr = [];
+
+    //get the date 3 weeks ago
+    let threeWeeksAgo = new Date();
+    threeWeeksAgo.setDate(threeWeeksAgo.getDate() - 21);
+    const exactDate = threeWeeksAgo.toISOString().split("T")[0];
+
+    const result = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+      owner: owner,
+      repo: repo,
+      since: exactDate,
+      per_page: 100,
+    });
+
+    for (let data of result.data) {
+      //skip the merge pull request commits
+      if (data.commit.verification.verified === true) {
+        continue;
+      }
+
+      let filteredData = reworkFilter(owner, repo, data.sha);
+
+      if (filteredData !== undefined) {
+        filteredDataArr.push(filteredData);
+      }
+    }
+
+    // res.status(200).json(filteredData);
+
+    Promise.all(filteredDataArr).then((stat) => {
+        res.send(stat);
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "get request for commit data failed exception" });
+    next(err);
+  }
+});
 
 module.exports = router;
